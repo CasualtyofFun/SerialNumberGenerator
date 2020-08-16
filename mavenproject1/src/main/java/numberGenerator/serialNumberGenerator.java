4a42225694196abfdd07cf89ffe4f5fd83564e8c
CasualtyofFun <kcall92@gmail.com>
2020-08-15 23:27:23 -0400

    Now gives serial number!

--- mavenproject1/src/main/java/numberGenerator/serialNumberGenerator.java
+++ mavenproject1/src/main/java/numberGenerator/serialNumberGenerator.java
@@ -1,5 +1,7 @@
 package numberGenerator;
 
+import java.time.LocalDate;
+import java.util.Random;
 import org.apache.commons.lang.StringUtils;
 
 
@@ -7,12 +9,31 @@ import org.apache.commons.lang.StringUtils;
 public class serialNumberGenerator {
 
     private baseConverter base;
-
+    private int largestNumber = 1679615;
+    
     public serialNumberGenerator() {
         base = new baseConverter(36);
+        
+    }
+    
+    public String generateSerialNumber(int jobNumber, LocalDate date){
+        StringBuilder b = new StringBuilder();
+        b.append(this.generateJobPrefix(jobNumber));
+        System.out.println("After Job Prefix there are " + b.toString().length()+ " characters");
+        b.append(this.generateDatePrefix(date));
+        System.out.println("After Date Prefix there are " + b.toString().length()+ " characters");
+        b.append(this.generateRandomPostfix());
+        System.out.println("After Postfix there are " + b.toString().length()+ " characters");
+        return b.toString();
+    }
+    
+    private String generateRandomPostfix(){
+        Random rand = new Random();
+        int r = rand.nextInt(largestNumber);   
+        return base.fromDeci(r);
     }
 
-    public String generateJobPrefix(int jobNumber) {
+    private String generateJobPrefix(int jobNumber) {
 
         String s = base.fromDeci(jobNumber);
         if(s.length() < 5){
@@ -21,6 +42,26 @@ public class serialNumberGenerator {
         
         return s;
     }
+    
+    private int dateToUseable(LocalDate date){
+        int month = date.getMonthValue(); int day = date.getDayOfMonth(); int year = date.getYear();
+        System.out.println(day);
+        System.out.println(month);
+        System.out.println(year);
+        StringBuilder b = new StringBuilder();
+        year = year - 2000;
+        System.out.println(year);
+        b.append(month); b.append(day); b.append(year);
+        return Integer.parseInt(b.toString());
+    }
+    
+    private String generateDatePrefix(LocalDate date) {
+
+        String s = base.fromDeci(dateToUseable(date));
+        
+        return s;
+    }
+    
     
     
 
--- mavenproject1/src/main/java/numberGenerator/test.java
+++ mavenproject1/src/main/java/numberGenerator/test.java
@@ -1,5 +1,7 @@
 package numberGenerator;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Kyle
@@ -7,10 +9,13 @@ package numberGenerator;
 public class test {
         
    public static void main(String[] args){
-
+       int testNumber = 99999999;
+       LocalDate date;
+       baseConverter base = new baseConverter(36);
        serialNumberGenerator gen = new serialNumberGenerator();
-       String s = gen.generateJobPrefix(123150);
+       String s = gen.generateSerialNumber(testNumber, LocalDate.of(2021, 10, 31));
        System.out.println(s);             
+       
                
                
                
